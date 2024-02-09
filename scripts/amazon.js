d96d583f038c4b79235bbc46f229c009cfccf4d6
zTwigo <thetwigo4@gmail.com>
2024-02-09 20:59:12 +0100

    Yes

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,37 @@
 // Array containing the information of the products (data structure)
 const products = [{
-    image : "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name : "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating : {
-        stars : 4.5,
-        count : 87
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+        stars: 4.5,
+        count: 87
     },
     // Best practice to use cents instead of dollars/euros, because js has some problems with decimal numbers
-    priceCents : 1090
+    priceCents: 1090
 }, {
-    image : "images/products/intermediate-composite-basketball.jpg",
-    name : "Intermediate Size Basketball",
-    rating : {
-        stars : 4,
-        count : 127
+    image: "images/products/intermediate-composite-basketball.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+        stars: 4,
+        count: 127
     },
-    priceCents : 2095
+    priceCents: 2095
 }, {
-    image : "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name : "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating : {
-        stars : 4.5,
-        count : 56  
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+        stars: 4.5,
+        count: 56  
     },
-    priceCents : 799
+    priceCents: 799
+}, {
+    image: "images/products/black-2-slot-toaster.jpg",
+    name: "2 Slot Toaster - Black",
+    rating: {
+        stars: 5,
+        count: 2197
+    },
+    priceCents: 1899
 }];
 
 // Get the product grid element
