30535931fe34a89ab7f137240d43d30aca413a72
zTwigo <thetwigo4@gmail.com>
2025-04-20 18:56:50 +0200

    Added a util function which checks for some parameters to be within the URL of the page

---
+++ scripts/utils/url.js
@@ -0,0 +1,24 @@
+/**
+ * This method will check for some parameters to be present
+ * within the current URL of the page.
+ * 
+ * @param parameters
+ *  
+ * @returns boolean
+ */
+export default function checkQueryParams(parameters){
+    // Value to be returned
+    let ret = true;
+
+    // Get the url of the page
+    const url = new URL(window.location.href);
+
+    // Loop through the array and check for the params
+    // to be within the URL
+    parameters.forEach(param => {
+        if(url.searchParams.get(param) == null) ret = false;
+    });
+
+    // Return the value
+    return ret;
+}
